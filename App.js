9e29166fe3da18dfc269a6005e1a79deee0050f5
cendekialnazalia <cendekialnazalia@gmail.com>
2022-09-19 11:35:11 +0700

    merubah informasi default dari welcome react native

--- App.js
+++ App.js
@@ -79,16 +79,6 @@ const App: () => Node = () => {
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
         </View>
       </ScrollView>
     </SafeAreaView>
