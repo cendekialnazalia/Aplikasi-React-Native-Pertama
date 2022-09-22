d796f47a3283e0357c08500149a7dfb3401244fb
cendekialnazalia <cendekialnazalia@gmail.com>
2022-09-22 12:06:08 +0700

    Adding Class Component Example

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Text, View, Image, TextInput} from 'react-native';
 
 const App = () => {
@@ -9,23 +9,48 @@ const App = () => {
       <Luthfieta />
       <Text>Nazalia</Text>
       <Photo />
-
       <TextInput style={{borderWidth: 1}} />
+      <BoxGreen />
+      <Profile />
     </View>
   );
 };
 
+//Functional Component
 const Luthfieta = () => {
   return <Text>Luthfieta Mangkuningrat</Text>;
 };
 
+//Functional Component
 const Photo = () => {
   return (
-    <Image
-      source={{uri: 'https://placeimg.com/640/480/arch'}}
-      style={{width: 100, height: 100}}
-    />
+    <View>
+      <Image
+        source={{uri: 'https://placeimg.com/640/480/arch'}}
+        style={{width: 100, height: 100, borderRadius: 50}}
+      />
+      <Text style={{color: 'blue', fontSize: 24}}>Ini Hewan</Text>
+    </View>
   );
 };
+
+//Class Component
+class BoxGreen extends Component {
+  render() {
+    return <Text>Ini component dari class</Text>;
+  }
+}
+
+//Class Component
+class Profile extends Component {
+  render() {
+    return (
+      <Image
+        source={{uri: 'https://placeimg.com/640/480/animals'}}
+        style={{width: 100, height: 100}}
+      />
+    );
+  }
+}
 
 export default App;
