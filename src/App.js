4869b11a16a96aa631c210f46038dd4c52df903d
cendekialnazalia <cendekialnazalia@gmail.com>
2022-10-18 14:57:30 +0700

    lifecyle class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, ScrollView} from 'react-native';
 import FlexBox from './pages/FlexBox';
 import Position from './pages/Position';
@@ -6,13 +6,19 @@ import SampleComponent from './pages/SampleComponent';
 import StylingComponent from './pages/StylingComponent';
 
 const App = () => {
+  const [isShow, SetIsShow] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      SetIsShow(false);
+    }, 6000);
+  }, []);
   return (
     <View>
       <ScrollView>
-        <SampleComponent />
-        <StylingComponent />
-        <FlexBox />
-        <Position />
+        {/* <SampleComponent /> */}
+        {/* <StylingComponent /> */}
+        {isShow && <FlexBox />}
+        {/* <Position /> */}
       </ScrollView>
     </View>
   );
--- src/pages/FlexBox/index.js
+++ src/pages/FlexBox/index.js
@@ -5,7 +5,30 @@ import {Text, View, Image} from 'react-native';
  * [Description FlexBox]
  */
 class FlexBox extends Component {
+  constructor(props) {
+    super(props);
+    console.log('==> constructor');
+    this.state = {
+      subsriber: 200,
+    };
+  }
+  componentDidMount() {
+    console.log('==> component did mount');
+    setTimeout(() => {
+      this.setState({
+        subsriber: 400,
+      });
+    }, 2000);
+  }
+  componentDidUpdate() {
+    console.log('==> Component did update');
+  }
+
+  componentWillUnmount() {
+    console.log('==> Component will unmount');
+  }
   render() {
+    console.log('==> render');
     return (
       <View>
         <View
@@ -40,7 +63,7 @@ class FlexBox extends Component {
             <Text style={{fontSize: 20, fontWeight: 'bold'}}>
               Cendekia Luthfieta Nazalia
             </Text>
-            <Text>100 ribu subcriber</Text>
+            <Text>{this.state.subsriber} ribu subcriber</Text>
           </View>
         </View>
       </View>
